a8cd741f6f5663dd6dad31b16d2ed3673101d965
Westindiess <gregoryalexiss@gmail.com>
2022-03-26 18:04:12 +0100

    app component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+
+
+const App = () => {
+  return (
+    <div className="bg-main min-h-screen">
+      <NavBar/>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
