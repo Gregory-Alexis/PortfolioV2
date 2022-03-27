61faa23024001c34a865ab1d367c5729bcab6f47
Westindiess <gregoryalexiss@gmail.com>
2022-03-27 21:06:17 +0200

    app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Header from "./components/Header";
+import Main from "./components/Main";
 import NavBar from "./components/NavBar";
 
 
 const App = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <div className="bg-main min-h-screen">
-      <NavBar/>
+      <NavBar isActive={isActive} setIsActive={setIsActive}/>
       <Header />
+
     </div>
   );
 }
