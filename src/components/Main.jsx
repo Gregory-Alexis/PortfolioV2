ac684032aaf8e517d8887bac578007e570dbe012
Westindiess <gregoryalexiss@gmail.com>
2022-03-27 21:07:16 +0200

    home main

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="p-4 text-main">
+      <h1 className=" border-b-2 w-48">Qui suis-je ?</h1>
+    </div>
+  );
+};
+
+export default Main;
