0404052a020ba89ecf11443b969c2ddc497c0518
Westindiess <gregoryalexiss@gmail.com>
2022-03-27 21:07:23 +0200

    home navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,53 @@
 import Menu from "../images/list.svg";
 import Fav from "../images/fav.png";
+import { useEffect, useRef, useState } from "react";
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+  const [active, setActive] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+  const ref = useRef();
+
+  const toggleHandler = () => {
+    setToggle(!toggle);
+  };
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      if (toggle && ref.current && !ref.current.contains(e.target)) {
+        setToggle(false);
+      }
+    };
+
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [toggle, setToggle]);
+
   return (
-    <div className="p-4 shadow-md flex justify-between">
+    <div className="p-4 shadow-md flex justify-between" ref={ref}>
       <img src={Fav} alt="favicone" />
       <h1 className="text-main text-xl">
         Grégory <span className="text-special">ALEXIS</span>
       </h1>
-      <img src={Menu} alt="menu" />
+      <button type="button" aria-label="toggle" onClick={toggleHandler}>
+        <img src={Menu} alt="menu" />
+      </button>
+
+      <div
+        className={`bg-main absolute h-screen z-20 w-72 left-0 top-0  transition-all duration-300 ease-in-out ${
+          toggle ? "-translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <ul className="flex flex-col justify-center items-center h-full">
+          <li className="phoneNavbarLink">Home</li>
+          <li className="phoneNavbarLink">Projets</li>
+          <li className="phoneNavbarLink">Qui suis-je ?</li>
+          <li className="phoneNavbarLink">Mes compétences</li>
+        </ul>
+      </div>
     </div>
   );
 };
