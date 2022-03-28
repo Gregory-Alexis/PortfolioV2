bd8c14f6bb0fb5c9ffa338b83f46ff4936717b98
Westindiess <gregoryalexiss@gmail.com>
2022-03-28 17:41:18 +0200

    Google font project

---
+++ src/components/ProjetComponent/GoogleFontApp.jsx
@@ -0,0 +1,16 @@
+import GoogleFont from "../../images/Font.png";
+import ProjectCustom from "./ProjectCustom";
+
+const GoogleFontApp = () => {
+  return (
+    <ProjectCustom
+      src={GoogleFont}
+      alt="Google Font"
+      hrefNetlify="https://googlefont.netlify.app/"
+      hrefGithub="https://github.com/Westindiess/TS-Google-Widget"
+      titre="Google Font"
+    />
+  );
+};
+
+export default GoogleFontApp;
