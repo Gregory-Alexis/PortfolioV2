78d7365eef10d895b6d9fb8f2906aa8028fe7d31
Westindiess <gregoryalexiss@gmail.com>
2022-03-28 17:41:50 +0200

    movie app

---
+++ src/components/ProjetComponent/MovieApp.jsx
@@ -0,0 +1,16 @@
+import Particeep from "../../images/Particeep.png";
+import ProjectCustom from "./ProjectCustom";
+
+const MovieApp = () => {
+  return (
+    <ProjectCustom
+      src={Particeep}
+      alt="Movie App"
+      hrefNetlify="https://movie-particeep-project.netlify.app/"
+      hrefGithub="https://github.com/Westindiess/Particeep-project"
+      titre="Movie App"
+    />
+  );
+};
+
+export default MovieApp;
