90272fc178e7c5bba85c1638adfb62a3ab97a4e7
Westindiess <gregoryalexiss@gmail.com>
2022-03-28 17:41:39 +0200

    Gradient project

---
+++ src/components/ProjetComponent/GradientApp.jsx
@@ -0,0 +1,16 @@
+import Gradients from "../../images/Gradients.png";
+import ProjectCustom from "./ProjectCustom";
+
+const GradientApp = () => {
+  return (
+    <ProjectCustom
+      src={Gradients}
+      alt="Gradient"
+      hrefNetlify="https://gradientproject.netlify.app/"
+      hrefGithub="https://github.com/Westindiess/TS-Gradient-Redux"
+      titre="Gradients"
+    />
+  );
+};
+
+export default GradientApp;
