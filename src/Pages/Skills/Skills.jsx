79ee731bbe6fd3d237bae67b7b9c7d867feb23d0
Westindiess <gregoryalexiss@gmail.com>
2022-03-30 16:44:58 +0200

    my skills

---
+++ src/Pages/Skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+import ReactSkills from "../../components/SkillsComponent/ReactSkills";
+
+const Skills = () => {
+  return (
+    <div className="bg-main min-h-screen">
+      <NavBar />
+      <ReactSkills />
+    </div>
+  );
+};
+
+export default Skills;
